f665e42971962babf7ad2303097cf67ede44972b
Jérémy VALENTIN <eresnova@neofrag.fr>
2020-04-13 17:18:29 +0200

    [Theme][Azuro] Remove .card-default (useless)

--- neofrag/install/alpha_0_2_1_1.php
+++ neofrag/install/alpha_0_2_1_1.php
@@ -16,5 +16,31 @@ class Alpha_0_2_1_1 extends Install
 
 		$this->db	->execute('ALTER TABLE `neofrag`.`nf_user` DROP INDEX `username`, ADD INDEX `username` (`username`) USING BTREE')
 					->execute('ALTER TABLE `neofrag`.`nf_user` DROP INDEX `email`, ADD INDEX `email` (`email`) USING BTREE');
+
+		foreach ($this->db()->select('disposition_id', 'disposition')->from('nf_dispositions')->where('theme', 'azuro')->where('disposition LIKE', '%card-default%')->get() as $disposition)
+		{
+			$disposition['disposition'] = unserialize($disposition['disposition']);
+
+			$disposition['disposition']->each($f = function($a) use (&$f){
+				if (is_a($a, 'NF\NeoFrag\Displayables\Widget'))
+				{
+					if ($a->style() == 'card-default')
+					{
+						$a->style('');
+					}
+				}
+				else if ($a)
+				{
+					$a->each($f);
+				}
+
+				return $a;
+			});
+
+			$this->db()	->where('disposition_id', $disposition['disposition_id'])
+						->update('nf_dispositions', [
+							'disposition' => serialize($disposition['disposition'])
+						]);
+		}
 	}
 }
